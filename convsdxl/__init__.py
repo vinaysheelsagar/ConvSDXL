7b7245a487615ef157d54e6325de8fae7112d9c5
Vinay Sagar <vine.green12@gmail.com>
2024-01-30 00:48:41 +0530

    changes to class from simple functions

--- convsdxl/__init__.py
+++ convsdxl/__init__.py
@@ -5,82 +5,11 @@ from .enums import *
 from PIL import Image
 from datetime import datetime
 from diffusers import DiffusionPipeline, AutoencoderKL
+from diffusers import StableDiffusionUpscalePipeline
 from diffusers.pipelines.stable_diffusion_xl.pipeline_stable_diffusion_xl import StableDiffusionXLPipeline
+from diffusers import StableDiffusionInpaintPipeline
 from diffusers.pipelines.stable_diffusion_xl.pipeline_stable_diffusion_xl_img2img import (
     StableDiffusionXLImg2ImgPipeline)
-
-
-_base: StableDiffusionXLPipeline | None = None
-_refiner: StableDiffusionXLImg2ImgPipeline | None = None
-
-
-def _clear_garbage():
-    gc.collect()
-
-
-def set_base(
-        model: str = "stabilityai/stable-diffusion-xl-base-1.0",
-        vae: str = "madebyollin/sdxl-vae-fp16-fix",
-        base=None,
-):
-    global _base
-
-    if base is not None:
-        _base = base
-        return
-
-    _clear_garbage()
-
-    vae = AutoencoderKL.from_pretrained(vae, torch_dtype=torch.float16)
-
-    _base = DiffusionPipeline.from_pretrained(
-        model,
-        vae=vae,
-        torch_dtype=torch.float16,
-        variant="fp16",
-        use_safetensors=True
-    )
-    _base.to("cuda")
-
-
-def remove_base():
-    global _base
-    _base = None
-    torch.cuda.empty_cache()
-    _clear_garbage()
-
-
-def set_refiner(
-        model: str = "stabilityai/stable-diffusion-xl-refiner-1.0",
-        vae: str = "madebyollin/sdxl-vae-fp16-fix",
-        refiner=None,
-):
-    global _refiner
-
-    if refiner is not None:
-        _refiner = refiner
-        return
-
-    _clear_garbage()
-
-    vae = AutoencoderKL.from_pretrained(vae, torch_dtype=torch.float16)
-
-    _refiner = DiffusionPipeline.from_pretrained(
-        model,
-        vae=vae,
-        torch_dtype=torch.float16,
-        variant="fp16",
-        use_safetensors=True
-    )
-    _refiner.to("cuda")
-    _refiner()
-
-
-def remove_refiner():
-    global _refiner
-    _refiner = None
-    torch.cuda.empty_cache()
-    _clear_garbage()
 
 
 def _clean_prompt(prompt: str):
@@ -88,13 +17,20 @@ def _clean_prompt(prompt: str):
     return cleaned_prompt
 
 
+def _clear_garbage():
+    gc.collect()
+
+
 def _save_image(image, image_dir):
     image_path = os.path.join(image_dir, f"{datetime.now()}.png")
     image.save(image_path, "PNG")
 
 
-def add_design_to_prompt(design_type: DesignType, prompt: str | None = None, negative_prompt: str | None = None):
-
+def add_design_to_prompt(
+        design_type: DesignType,
+        prompt: str | None = None,
+        negative_prompt: str | None = None,
+):
     if prompt is None:
         prompt = ""
 
@@ -129,22 +65,16 @@ def add_design_to_prompt(design_type: DesignType, prompt: str | None = None, neg
     return designed_prompt, designed_negative_prompt
 
 
-def get_image(
-        prompt: str,
-        negative_prompt: str = None,
-        num_inference_steps: int = 25,
-        design_type: DesignType = None,
-        image_dir: str = None,
+def preprocess_prompts(
+        design_type: DesignType | None,
+        prompt: str | None,
+        negative_prompt: str | None,
         **model_kwargs
 ):
-    global _base
+    if prompt is not None:
+        prompt = _clean_prompt(prompt)
+        model_kwargs["prompt"] = prompt
 
-    os.makedirs(image_dir, exist_ok=True)
-
-    if _base is None:
-        set_base()
-
-    prompt = _clean_prompt(prompt)
     if negative_prompt is not None:
         negative_prompt = _clean_prompt(negative_prompt)
         model_kwargs["negative_prompt"] = negative_prompt
@@ -156,62 +86,254 @@ def get_image(
             design_type=design_type,
         )
 
-        prompt = designed_prompts[0]
-        negative_prompt = designed_prompts[1]
-        model_kwargs["negative_prompt"] = negative_prompt
+        model_kwargs["prompt"] = designed_prompts[0]
+        model_kwargs["negative_prompt"] = designed_prompts[1]
 
-    image = _base(
-        prompt=prompt,
-        num_inference_steps=num_inference_steps,
-        **model_kwargs
-    ).images[0]
-
-    if image_dir is not None:
-        _save_image(image, image_dir)
-
-    return image
+    return model_kwargs
 
 
-def refine_image(
-        image: Image,
-        prompt: str,
-        negative_prompt: str = None,
-        num_inference_steps: int = 25,
-        design_type: DesignType = None,
-        image_dir: str = None,
-        **model_kwargs,
-):
-    global _refiner
+class ConvSDXL:
+    _base: StableDiffusionXLPipeline | None = None
+    _refiner: StableDiffusionXLImg2ImgPipeline | None = None
+    _inpaint: StableDiffusionXLPipeline | None = None
+    _upscaler: StableDiffusionUpscalePipeline | None = None
 
-    os.makedirs(image_dir, exist_ok=True)
+    def set_base(
+            self,
+            model: str = "stabilityai/stable-diffusion-xl-base-1.0",
+            # vae: str = "madebyollin/sdxl-vae-fp16-fix",
+            base=None,
+    ):
 
-    if _refiner is None:
-        set_refiner()
+        if base is not None:
+            self._base = base
+            return
 
-    prompt = _clean_prompt(prompt)
-    if negative_prompt is not None:
-        negative_prompt = _clean_prompt(negative_prompt)
-        model_kwargs["negative_prompt"] = negative_prompt
+        _clear_garbage()
 
-    if design_type is not None:
-        designed_prompts = add_design_to_prompt(
+        # vae = AutoencoderKL.from_pretrained(vae, torch_dtype=torch.float16)
+
+        self._base = DiffusionPipeline.from_pretrained(
+            model,
+            # vae=vae,
+            torch_dtype=torch.float16,
+            variant="fp16",
+            use_safetensors=True
+        )
+        self._base.to("cuda")
+
+    def remove_base(self):
+        self._base = None
+        torch.cuda.empty_cache()
+        _clear_garbage()
+
+    def set_inpaint(
+            self,
+            model: str = "stabilityai/stable-diffusion-2-inpainting",
+            # vae: str = "madebyollin/sdxl-vae-fp16-fix",
+            inpaint=None,
+    ):
+
+        if inpaint is not None:
+            self._inpaint = inpaint
+            return
+
+        _clear_garbage()
+
+        # vae = AutoencoderKL.from_pretrained(vae, torch_dtype=torch.float16)
+
+        self._inpaint = StableDiffusionInpaintPipeline.from_pretrained(
+            model,
+            # vae=vae,
+            torch_dtype=torch.float16,
+            variant="fp16",
+            use_safetensors=True
+        )
+        self._inpaint.to("cuda")
+
+    def remove_inpaint(self):
+        self._inpaint = None
+        torch.cuda.empty_cache()
+        _clear_garbage()
+
+    def set_refiner(
+            self,
+            model: str = "stabilityai/stable-diffusion-xl-refiner-1.0",
+            # vae: str = "madebyollin/sdxl-vae-fp16-fix",
+            refiner=None,
+    ):
+
+        if refiner is not None:
+            self._refiner = refiner
+            return
+
+        _clear_garbage()
+
+        # vae = AutoencoderKL.from_pretrained(vae, torch_dtype=torch.float16)
+
+        self._refiner = DiffusionPipeline.from_pretrained(
+            model,
+            # vae=vae,
+            torch_dtype=torch.float16,
+            variant="fp16",
+            use_safetensors=True
+        )
+        self._refiner.to("cuda")
+
+    def remove_refiner(self):
+        self._refiner = None
+        torch.cuda.empty_cache()
+        _clear_garbage()
+
+    def set_upscaler(
+            self,
+            model: str = "stabilityai/stable-diffusion-x4-upscaler",
+            # vae: str = "madebyollin/sdxl-vae-fp16-fix",
+            upscaler=None,
+    ):
+
+        if upscaler is not None:
+            self._upscaler = upscaler
+            return
+
+        _clear_garbage()
+
+        # vae = AutoencoderKL.from_pretrained(vae, torch_dtype=torch.float16)
+
+        self._refiner = DiffusionPipeline.from_pretrained(
+            model,
+            # vae=vae,
+            torch_dtype=torch.float16,
+            variant="fp16",
+            use_safetensors=True
+        )
+        self._refiner.to("cuda")
+
+    def remove_upscaler(self):
+        self._refiner = None
+        torch.cuda.empty_cache()
+        _clear_garbage()
+
+    def get_image(
+            self,
+            prompt: str,
+            negative_prompt: str = None,
+            design_type: DesignType = None,
+            image_dir: str | os.PathLike = None,
+            **model_kwargs
+    ):
+
+        os.makedirs(image_dir, exist_ok=True)
+
+        if self._base is None:
+            self.set_base()
+
+        model_kwargs = preprocess_prompts(
             design_type=design_type,
             prompt=prompt,
             negative_prompt=negative_prompt,
         )
 
-        prompt = designed_prompts[0]
-        negative_prompt = designed_prompts[1]
-        model_kwargs["negative_prompt"] = negative_prompt
+        image = self._base(
+            **model_kwargs
+        ).images[0]
 
-    image = _refiner(
-        image=image,
-        prompt=prompt,
-        num_inference_steps=num_inference_steps,
-        **model_kwargs,
-    ).images[0]
+        if image_dir is not None:
+            _save_image(image, image_dir)
 
-    if image_dir is not None:
-        _save_image(image, image_dir)
+        return image
 
-    return image
+    def inpaint_image(
+            self,
+            prompt: str,
+            image: Image,
+            mask_image: Image,
+            negative_prompt: str = None,
+            design_type: DesignType = None,
+            image_dir: str | os.PathLike = None,
+            **model_kwargs,
+    ):
+
+        os.makedirs(image_dir, exist_ok=True)
+
+        if self._inpaint is None:
+            self.set_inpaint()
+
+        model_kwargs = preprocess_prompts(
+            design_type=design_type,
+            prompt=prompt,
+            negative_prompt=negative_prompt,
+        )
+
+        image = self._inpaint(
+            image=image,
+            mask_image=mask_image,
+            **model_kwargs
+        ).images[0]
+
+        if image_dir is not None:
+            _save_image(image, image_dir)
+
+        return image
+
+    def refine_image(
+            self,
+            image: Image,
+            prompt: str,
+            negative_prompt: str = None,
+            design_type: DesignType = None,
+            image_dir: str | os.PathLike = None,
+            **model_kwargs,
+    ):
+
+        os.makedirs(image_dir, exist_ok=True)
+
+        if self._refiner is None:
+            self.set_refiner()
+
+        model_kwargs = preprocess_prompts(
+            design_type=design_type,
+            prompt=prompt,
+            negative_prompt=negative_prompt,
+        )
+
+        image = self._refiner(
+            image=image,
+            **model_kwargs,
+        ).images[0]
+
+        if image_dir is not None:
+            _save_image(image, image_dir)
+
+        return image
+
+    def upscale_image(
+            self,
+            image: Image,
+            prompt: str,
+            negative_prompt: str = None,
+            design_type: DesignType = None,
+            image_dir: str | os.PathLike = None,
+            **model_kwargs,
+    ):
+
+        os.makedirs(image_dir, exist_ok=True)
+
+        if self._refiner is None:
+            self.set_refiner()
+
+        model_kwargs = preprocess_prompts(
+            design_type=design_type,
+            prompt=prompt,
+            negative_prompt=negative_prompt,
+        )
+
+        image = self._refiner(
+            image=image,
+            **model_kwargs,
+        ).images[0]
+
+        if image_dir is not None:
+            _save_image(image, image_dir)
+
+        return image
